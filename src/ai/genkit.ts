89d9b9aa090fbe4d36f508b68a700e15684d650c
Tyrone Robb <founders@interseed.ai>
2025-06-26 16:06:44 +0000

    should we not be using vertex?

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,11 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-2.0-flash',
+  plugins: [
+    googleAI({
+      projectId: process.env.GOOGLE_CLOUD_PROJECT,
+      location: process.env.GOOGLE_CLOUD_LOCATION,
+    }),
+  ],
+  model: 'gemini-1.5-flash-001',
 });
