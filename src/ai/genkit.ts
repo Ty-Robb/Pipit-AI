3c46b2a848c5daf6453fea0227d42c6ebd255f0d
Tyrone Robb <founders@interseed.ai>
2025-06-26 16:17:36 +0000

    GenkitError: FAILED_PRECONDITION: Please pass in the API key or set the

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,13 +1,10 @@
-import {config} from 'dotenv';
-config();
-
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
     googleAI({
-      projectId: process.env.GOOGLE_CLOUD_PROJECT,
+      projectId: process.env.GCLOUD_PROJECT,
       location: process.env.GOOGLE_CLOUD_LOCATION || 'us-central1',
     }),
   ],
