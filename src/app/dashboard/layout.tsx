93403a83400e8a135a780cc6027657018e3724bf
Tyrone Robb <founders@interseed.ai>
2025-06-27 15:45:24 +0000

    fix: resolve sidebar provider error on dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+"use client"
+import { SidebarProvider } from "@/components/ui/sidebar";
+
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return <SidebarProvider>{children}</SidebarProvider>
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,13 +10,12 @@ import {
 import { Separator } from "@/components/ui/separator"
 import {
   SidebarInset,
-  SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
 
 export default function Page() {
   return (
-    <SidebarProvider>
+    <>
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2">
@@ -47,6 +46,6 @@ export default function Page() {
           <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
         </div>
       </SidebarInset>
-    </SidebarProvider>
+    </>
   )
 }
